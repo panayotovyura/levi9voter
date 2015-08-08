41686df414dd0aab7ee3fd05780459e82f4508d9
Nikolay Labinskiy <e-moe@ukr.net>
2015-08-08 10:30:27 +0300

    vote

---
+++ src/AppBundle/Entity/Vote.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Vote
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Vote
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="vote", type="integer")
+     */
+    private $vote;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set vote
+     *
+     * @param integer $vote
+     * @return Vote
+     */
+    public function setVote($vote)
+    {
+        $this->vote = $vote;
+
+        return $this;
+    }
+
+    /**
+     * Get vote
+     *
+     * @return integer 
+     */
+    public function getVote()
+    {
+        return $this->vote;
+    }
+}
